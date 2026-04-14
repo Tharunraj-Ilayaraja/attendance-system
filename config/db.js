20bd08787a8d12dfbce66f96340336d44592429f
Tharunraj <230701362@rajalakshmi.edu.in>
2026-04-14 23:41:01 +0530

    Database done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
+
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
-  database: "attendance_system",
-  password: "postgres",
+  host: "attendance-system-db-tr.postgres.database.azure.com",
+  database: "attendance-system", 
+  password: "Raja@1978",
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 
 
 
-
 const cors = require("cors");
 //app.use(cors());
 app.use(cors({
